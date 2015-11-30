2355837d610fb4e27b0f19bf1cbd097980bedc8f
Clodéric Mars <cloderic.mars@craft.ai>
2015-11-30 17:53:17 +0100

    Now using the official craft ai client js

--- src/view/containers/app.jsx
+++ src/view/containers/app.jsx
@@ -1,16 +1,15 @@
 import '../static/circle.css';
+import { ActionsStore, devices } from '../../actions/actionsStore';
+import { ButtonGroup, Button, Modal, Input, Grid, Row, Col } from 'react-bootstrap';
+import { registerActions, MapOfTime } from '../../actions/actions';
+import { SH_USER, SH_PROJECT, SH_VERSION, SH_APP_ID, SH_APP_SECRET } from '../../constant';
 import ChatHistory from '../components/chatHistory';
 import Clock from '../components/clock';
+import craftai from 'craft-ai-client-js';
 import Meter from '../components/meter';
 import Planning from '../components/planning';
 import React from 'react';
 import Reflux from 'reflux';
-import { ButtonGroup, Button, Modal, Input, Grid, Row, Col } from 'react-bootstrap';
-import * as Runtime from '../../runtime';
-import { SH_USER, SH_PROJECT, SH_VERSION, SH_APP_ID, SH_APP_SECRET } from '../../constant';
-import { registerActions, MapOfTime } from '../../actions/actions';
-import { ActionsStore, devices } from '../../actions/actionsStore';
-
 
 export default React.createClass({
   mixins: [Reflux.connect(ActionsStore, 'actionsStore')],
@@ -18,11 +17,8 @@ export default React.createClass({
     return {  simID:'', showModal:true, ready:false, appSecret:'', appID:'', ghUser:'', ghProject:'', ghBranch:'' }
   },
   handlePresence: function() {
-    //Runtime.updateEntityKnowledge(0, 'sensors', {presence: event.target.checked });
     devices.setPresence( event.target.checked );
-    //console.log("presence state = " + event.target.checked);
   },
-  
   updateTemperature: function(event) {
     var val = parseInt(event.target.value);
     devices.setTemperature(val);
@@ -30,10 +26,10 @@ export default React.createClass({
   addTime: function( amount,check ) {
 
     if(check && !this.state.actionsStore.waitBT ) {
-      devices.addTime(amount,check); 
+      devices.addTime(amount,check);
     }
     if( !check ) {
-      devices.addTime(amount,check); 
+      devices.addTime(amount,check);
     }
   },
   setDateTime: function( datetime ) {
@@ -55,30 +51,30 @@ export default React.createClass({
     if( this.state.actionsStore.disabledUI === false )
     {
       this.addTime(5*60*1000, true);
-    } 
+    }
     //var t = setTimeout(()=>{this.handleTime()},1000);
   },
   handleTimeInit: function(amount) {
     if( amount > 0 ) {
       this.addTime(30*1000,false);
-      setTimeout(()=>{this.handleTimeInit(amount-1)},500);  
+      setTimeout(()=>{this.handleTimeInit(amount-1)},500);
     }
   },
   getTime: function() {
     return this.state.actionsStore.time;
   },
   getPlanning: function() {
-    return Runtime.getEntityKnowledge(0)
-    .then((kb)=>{
-      return kb.knowledge.planning;
+    return this.instance.getAgentKnowledge(0)
+    .then(knowledge => {
+      return knowledge.planning;
     })
     .catch(()=>{
       console.log('unable to retrieve planning');
     });
   },
   getWorkdays: function() {
-    Runtime.getEntityKnowledge(0)
-    .then((kb)=>{
+    this.instance.getAgentKnowledge(0)
+    .then(knowledge =>{
       return [1,2,4,5];
     });
   },
@@ -86,7 +82,7 @@ export default React.createClass({
     return this.state.actionsStore.automaticTime || this.state.actionsStore.disabledUI;
   },
   start:function() {
-    
+
     this.setState({ showModal: false });
 
     var user = this.state.ghUser;
@@ -94,7 +90,7 @@ export default React.createClass({
     var branch = this.state.ghBranch;
     var appID = this.state.appID;
     var appSecret = this.state.appSecret;
-    if( this.state.ghUser === '' 
+    if( this.state.ghUser === ''
       &&this.state.ghProject === ''
       &&this.state.ghBranch === ''
       &&this.state.appID === ''
@@ -106,19 +102,32 @@ export default React.createClass({
       appSecret = SH_APP_SECRET;
     }
     else {
-      history.pushState(undefined, undefined, window.location.origin + window.location.pathname 
-      + '?owner=' + this.state.ghUser 
-      + '&project=' + this.state.ghProject 
-      + '&branch=' + this.state.ghBranch 
-      + '&appid=' + this.state.appID 
+      history.pushState(undefined, undefined, window.location.origin + window.location.pathname
+      + '?owner=' + this.state.ghUser
+      + '&project=' + this.state.ghProject
+      + '&branch=' + this.state.ghBranch
+      + '&appid=' + this.state.appID
       + '&appsecret=' + this.state.appSecret);
     }
 
-    Runtime.createSimulation( user, project, branch, appID, appSecret )
-    .then(() => {
-      this.setState( {simID: Runtime.simID} );
-      Runtime.doWS();
-      return registerActions();
+    craftai({
+      owner: user,
+      name: project,
+      version: branch,
+      appId: appID,
+      appSecret: appSecret
+    })
+    .then((instance) => {
+      this.instance = instance;
+      this.setState({
+        simID: instance.id
+      });
+
+      window.onbeforeunload = () => {
+        instance.destroy();
+      }
+
+      return registerActions(instance);
     })
     .then(() => {
       var planWD = [{time:6*60,value:20},
@@ -131,27 +140,33 @@ export default React.createClass({
                   {time:17*60,value:22},
                   {time:20*60,value:20},
                   {time:23*60,value:18}]
-      return Runtime.createEntity( 'agent/Robot.bt', {time:MapOfTime(ActionsStore.getTime()),planning:{workday:planWD,weekend:planWE}} );
+      return this.instance.createAgent( 'agent/Robot.bt', {
+        time: MapOfTime(ActionsStore.getTime()),
+        planning: {
+          workday: planWD,
+          weekend: planWE
+        }
+      });
     })
     .then(()=> {
       this.setState( {ready: true} );
       this.handleTimeInit(2);
-      Runtime.doUpdate();
-      console.log(Runtime.simID);
+      this.instance.update(0);
+      console.log(this.instance.id);
 
     })
     .catch((err)=>{
       console.log(err);
       this.setState({ showModal: true });
-    });    
+    });
   },
-  
+
   render: function() {
     if( this.state.actionsStore.automaticTime )
       this.handleTime();
     if( this.state.showModal )
       return (
-          <Grid>          
+          <Grid>
             <Row>
               <h2><img src="./favicons/craft-ai.gif"/> CRAFT NI Simulator</h2>
             </Row>
@@ -163,7 +178,7 @@ export default React.createClass({
             <Col xs={12} style={{backgroundColor:'#F0F8FF'}} >
             <br/>
             <p>In order to run this demo with your own BTs, you need to fork <a href='https://github.com/craft-ai/demo.SmartHome' target='_blank'>this</a> repository with your GitHub account</p>
-            <p>If you do not have a GitHub account, sign up for one <a href='https://github.com'  target='_blank'>here</a></p> 
+            <p>If you do not have a GitHub account, sign up for one <a href='https://github.com'  target='_blank'>here</a></p>
             <p>Then using <a href='https://workbench.craft.ai' target='_blank'>craft ai workbench</a>, you need to retrieve your appID/appSecret for this fork by editing the craft_project.json</p>
             <center><img src='./workbench.png' width='600px'/></center>
             <br/>
@@ -190,13 +205,13 @@ export default React.createClass({
           </Row>
           <Row>
             <h2><span className="glyphicon glyphicon-arrow-right" aria-hidden="true"></span> Starting runtime</h2>
-          </Row>          
+          </Row>
           <Row>
             <h3><img src="./favicons/load.gif"/>     Please wait....</h3>
           </Row>
           <Row>
           {this.state.simID === '' ?
-          void 0:          
+          void 0:
           <h3>           {this.state.simID} created</h3>
           }
           </Row>
@@ -206,7 +221,7 @@ export default React.createClass({
     else
 
     return (
-      <Grid>        
+      <Grid>
         <Row>
           <h2><img src="./favicons/favicon-32x32.png"/> CRAFT NI Simulator</h2>
         </Row>
@@ -216,14 +231,14 @@ export default React.createClass({
           </Col>
           <Col xs={6}>
             <form>
-              <ButtonGroup style={{ marginBottom: 10 }}>  
+              <ButtonGroup style={{ marginBottom: 10 }}>
                 <Button disabled={this.isUIDisabled()||this.state.actionsStore.heater} onClick={()=> devices.setHeater( true )}>Enable heater</Button>
                 <Button disabled={this.isUIDisabled()||!this.state.actionsStore.heater} onClick={()=> devices.setHeater( false )}>Disable heater</Button>
               </ButtonGroup>
               <div style={{ width: 180, marginBottom: 10 }}>
-                <Input disabled={this.isUIDisabled()} type='number' addonAfter="° (room temp.)" min={10} max={30} value={this.state.actionsStore.temperature} onChange={(evt) => this.updateTemperature(evt)}/>        
+                <Input disabled={this.isUIDisabled()} type='number' addonAfter="° (room temp.)" min={10} max={30} value={this.state.actionsStore.temperature} onChange={(evt) => this.updateTemperature(evt)}/>
               </div>
-              <ButtonGroup>  
+              <ButtonGroup>
                 <Button disabled={this.state.actionsStore.automaticTime} onClick={()=>this.startTime()} bsStyle="success">Start time</Button>
                 <Button disabled={!this.state.actionsStore.automaticTime} onClick={()=>this.stopTime()} bsStyle="danger">Stop time</Button>
               </ButtonGroup>
@@ -231,12 +246,12 @@ export default React.createClass({
             </form>
           </Col>
         </Row>
-        <Row> 
+        <Row>
           <Col xs={12}>
             <Clock disabled={this.isUIDisabled()} time={this.state.actionsStore.time} onTimeChange={(time) =>{this.setDateTime(time);}} />
           </Col>
         </Row>
-        <Row> 
+        <Row>
           <Col xs={12}>
             <ChatHistory id="hist" placeholder='No message...'/>
           </Col>
@@ -253,7 +268,7 @@ export default React.createClass({
     if (url.substr(-1) === '/') {
       url = url.substr(0, url.length - 1);
     }
-    var vars = url.split('&');    
+    var vars = url.split('&');
     //console.log(vars)
     var owner='';
     var repo='';
@@ -278,6 +293,7 @@ export default React.createClass({
   },
   componentWillUnmount: function() {
     console.log("unmount");
-    Runtime.destroySimulation({});
+    this.instance.destroy();
+    this.instance = undefined;
   }
 });
